ca0101e10398b8e158046ac0082a8ce3620d7cfc
Salvador <Sagarcia2713@gmail.com>
2022-01-17 16:50:40 -0600

    Set up the UI on the storyboard with 2 UISegmented controls, 2 Labels, and one UIImageView. Place one segmented control on top, followed by the imageView, then the bottom segmented control. The two UILabels are placed on the tge ImageView. One label will be on top and change depending on the value of the segmented control. The bottom label will correspond to the top segmented control. Connection to the UIElements have been made. Each UI Element has an IBOutlet and the two segmented controls are connected to an IBAction. A Surprised Pikachu face has been added to the Xcassets and has been set to the imageView's image property using Interface builder

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var topSegmentedControl: UISegmentedControl!
+    @IBOutlet var bottomSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var bottomLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func segmentedValueChanged(_ sender: UISegmentedControl) {
+    }
+    
 }
 
