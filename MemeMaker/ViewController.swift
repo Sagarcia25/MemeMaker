7dac36f5de913f8e4f767054a0ad455978fa12da
Salvador <Sagarcia2713@gmail.com>
2022-01-17 17:31:28 -0600

    Update the 2 arrays to contain the CaptionOption objects rather than an array of captions. Change the viewDidLoad to programmatically set up the Segmented controls. The Segmented controls will only display emojis as the options and will only populate as many options as there are in the respective choice array. Create a new function called update UI that will change the top and bottom label depending on what segment is selected on the UISegmentControls. Call this updateUI() function at the end of the ViewDidLoad and inside the IBAction of the SegmentedControl.

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -15,16 +15,43 @@ class ViewController: UIViewController {
     @IBOutlet var topLabel: UILabel!
     @IBOutlet var bottomLabel: UILabel!
     
-    let topChoices = ["When you tell your dog to stop barking","When you tell your dog to fetch the toy","When you tell your dog time to eat"]
-    let bottomChoices = ["The dog starts howling", "They get their toy and go to bed", "The dog takes your food away"]
+    
+    
+    let topChoices = [
+        CaptionOption(emoji: "🐕", caption: "When you tell your dog to stop barking"),
+        CaptionOption(emoji: "🧸", caption: "When you tell your dog to fetch the toy"),
+        CaptionOption(emoji: "🍗", caption: "When you tell your dog time to eat")
+                    ]
+    
+    let bottomChoices = [
+        CaptionOption(emoji: "😭", caption: "The dog starts howling instead"),
+        CaptionOption(emoji: "📱", caption: "They take your phone awy from your hand"),
+        CaptionOption(emoji: "🌮", caption: "The dog takes your food away")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topSegmentedControl.removeAllSegments()
+        bottomSegmentedControl.removeAllSegments()
+        for choice in topChoices{
+            topSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+        
+        for choice in bottomChoices {
+            bottomSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
+        }
+        bottomSegmentedControl.selectedSegmentIndex = 0
+        topSegmentedControl.selectedSegmentIndex = 0
+        updateUI()
     }
 
     @IBAction func segmentedValueChanged(_ sender: UISegmentedControl) {
+        updateUI()
     }
     
+    func updateUI(){
+        topLabel.text = topChoices[topSegmentedControl.selectedSegmentIndex].caption
+        bottomLabel.text = bottomChoices[bottomSegmentedControl.selectedSegmentIndex].caption
+    }
 }
 
